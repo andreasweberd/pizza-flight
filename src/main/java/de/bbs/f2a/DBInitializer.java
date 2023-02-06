5c4990465188ead41f6aa0e5a1bec2e27eb6d88d
Andreas Weber <andreas.weber@dedalus.com>
2023-02-06 15:00:34 +0100

    added datasource h2

---
+++ src/main/java/de/bbs/f2a/DBInitializer.java
@@ -0,0 +1,40 @@
+package de.bbs.f2a;
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import io.quarkus.runtime.StartupEvent;
+
+@ApplicationScoped
+public class DBInitializer {
+
+
+    @Inject
+    DataSource dataSource;
+
+    void onStart(@Observes StartupEvent ev) {
+        initdb();
+    }
+
+    private void initdb() {
+        System.out.println("Init DB");
+        try (Connection connection = dataSource.getConnection()) {
+            try (Statement stmt = connection.createStatement()) {
+                stmt.execute("CREATE TABLE users (name VARCHAR(255), password name VARCHAR(255))");
+                stmt.execute("INSERT INTO users('name', 'password') VALUES ('peter', )")
+            } catch (SQLException e) {
+                System.out.println("Error processing statement " + e);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error processing connection " + e);
+        }
+    }
+}
